039e89fffd6a79b68c53fb96b67257277b565349
withcolinsong <withcolinsong@gmail.com>
2021-06-12 23:05:02 +0900

    file 수정

--- src/vo/userInfo.java
+++ src/vo/userInfo.java
@@ -1,84 +1,100 @@
 package vo;
 
-import java.util.Scanner;
-
 public class userInfo {
+    private String id;              // 아이디
+    private String name;            // 이름
+    private String studyDate;       // 스터디날짜
+    private String chapter;         // 챕터
+    private String homework;        // 과제
+    private String middleMeetings;  // 중간점검참석
+    private String weekMeeting;     // 일주일점검참석
+    private String deposit;         // 보증금
+    private String joinDate;        // 가입날짜
+    private int fine;               // 벌금 -> 1회당 500원으로 측정함
 
-        private String userName;
-        private int deposit;
-        private String chapter;
-        private String homework;
-        private String middleMeetings;
-        private String weekMeetings;
-        private int studyFine;
-        private int num;
-
-        Scanner scan;
-
-        public void setInfo() {
-            System.out.print("이름 : ");
-            this.setUserName(scan.next());
-            System.out.print("보증금 : ");
-            this.setDeposit(scan.nextInt());
-            System.out.print("챕터정리 : ");
-            this.setChapter(scan.next());
-            System.out.print("과제 : ");
-            this.setHomework(scan.next());
-            System.out.print("중간점검참석 : ");
-            this.setMiddleMeetings(scan.next());
-            System.out.print("1주일점검참석 : ");
-            this.setWeekMeetings(scan.next());
-        }
-
-
-
-        public void setUserName(String userName) {
-            this.userName = userName;
-        }
-        public void setDeposit(int deposit) {
-            this.deposit = deposit;
-        }
-        public void setChapter(String chapter) {
-            this.chapter = chapter;
-        }
-        public void setHomework(String homework) {
-            this.homework = homework;
-        }
-        public void setMiddleMeetings(String middleMeetings) {
-            this.middleMeetings = middleMeetings;
-        }
-        public void setWeekMeetings(String weekMeetings) {
-            this.weekMeetings = weekMeetings;
-        }
-        public String getUserName() {
-            return userName;
-        }
-        public int getDeposit() {
-            return deposit;
-        }
-        public String getChapter() {
-            return chapter;
-        }
-        public String getHomework() {
-            return homework;
-        }
-        public String getMiddleMeetings() {
-            return middleMeetings;
-        }
-        public String getWeekMeetings() {
-            return weekMeetings;
-        }
-        public void setStudyFine(int studyFine) {
-            this.studyFine = studyFine;
-        }
-        public int getStudyFine() {
-            return studyFine;
-        }
-        public int getNum() {
-            return num;
-        }
-        public void setNum(int num) {
-            this.num = num;
-        }
+    @Override
+    public String toString() {
+        return "userInfo{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", studyDate='" + studyDate + '\'' +
+                ", chapter='" + chapter + '\'' +
+                ", homework='" + homework + '\'' +
+                ", middleMeetings='" + middleMeetings + '\'' +
+                ", weekMeeting='" + weekMeeting + '\'' +
+                ", deposit='" + deposit + '\'' +
+                ", joinDate='" + joinDate + '\'' +
+                ", fine=" + fine +
+                '}';
     }
 
+    public void setName(String name) { this.name = name; }
+
+    public void setStudyDate(String studyDate) { this.studyDate = studyDate; }
+
+    public void setChapter(String chapter) {
+        this.chapter = chapter;
+    }
+
+    public void setHomework(String homework) {
+        this.homework = homework;
+    }
+
+    public void setMiddleMeetings(String middleMeetings) {
+        this.middleMeetings = middleMeetings;
+    }
+
+    public void setWeekMeeting(String weekMeeting) {
+        this.weekMeeting = weekMeeting;
+    }
+
+    public void setDeposit(String deposit) {
+        this.deposit = deposit;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStudyDate() {
+        return studyDate;
+    }
+
+    public String getChapter() { return chapter; }
+
+    public String getHomework() {
+        return homework;
+    }
+
+    public String getMiddleMeetings() {
+        return middleMeetings;
+    }
+
+    public String getWeekMeeting() {
+        return weekMeeting;
+    }
+
+    public String getDeposit() {
+        return deposit;
+    }
+
+    public String getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(String joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public void setFine(int fine) {  this.fine = fine;   }
+    public int getFine() {  return fine; }
+}
